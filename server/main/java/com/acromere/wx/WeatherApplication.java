2e0c927db4a90fa5ee51ce77b3e1a20665142c64
Mark Soderquist <msoderquist@mastercontrol.com>
2026-03-16 17:05:46 -0600

    Start work on weather data server

---
+++ server/main/java/com/acromere/wx/WeatherApplication.java
@@ -0,0 +1,13 @@
+package com.acromere.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApplication {
+
+	static void main( String[] args ) {
+		SpringApplication.run( WeatherApplication.class, args );
+	}
+
+}
---
+++ server/test/java/com/acromere/wx/WeatherApplicationTests.java
@@ -0,0 +1,18 @@
+package com.acromere.wx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WeatherApplicationTests {
+
+	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner().withConfiguration( AutoConfigurations.of( WeatherApplication.class ) );
+
+	@Test
+	public void contextLoads() {
+		assertThat( contextRunner ).isNotNull();
+	}
+
+}
