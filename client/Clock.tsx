501ab3a7987a6c672c41ce9c0c792ca121b31c24
Mark Soderquist <msoderquist@mastercontrol.com>
2026-04-13 23:05:58 -0600

    Add zulu time to clock

--- client/Clock.tsx
+++ client/Clock.tsx
@@ -3,35 +3,39 @@ import {getDowName, pad} from "./Util.ts";
 
 export default function Clock() {
 
-  const [time, setTime] = useState('');
-  const [date, setDate] = useState('');
-  const [ap, setAp] = useState('');
-  const [dow, setDow] = useState('');
+  const [time, setTime] = useState('')
+  const [date, setDate] = useState('')
+  const [ap, setAp] = useState('')
+  const [dow, setDow] = useState('')
+  const [zulu, setZulu] = useState('')
 
   const updateTime = () => {
-    let timestamp = new Date();
-    let yy = timestamp.getFullYear();
-    let mo = timestamp.getMonth() + 1;
-    let dd = timestamp.getDate();
-    let hh = timestamp.getHours();
-    let mm = timestamp.getMinutes();
-    let ss = timestamp.getSeconds();
-    let ap = hh < 12 ? "am" : "pm";
-    let day = timestamp.getDay();
-    let dw = getDowName(day);
+    const timestamp = new Date();
+    const yy = timestamp.getFullYear();
+    const mo = timestamp.getMonth() + 1;
+    const dd = timestamp.getDate();
+    const hr = timestamp.getHours();
+    const mm = timestamp.getMinutes();
+    const ap = hr < 12 ? "am" : "pm";
+    const day = timestamp.getDay();
+    const dw = getDowName(day);
 
-    hh = hh % 12;
+    const utcHh = timestamp.getUTCHours();
+    const utcMm: number = timestamp.getUTCMinutes();
+
+    let hh = hr % 12;
     hh = hh === 0 ? 12 : hh;
 
     setTime(pad(hh, 2) + ':' + pad(mm, 2));
     setDate(pad(yy, 4) + '-' + pad(mo, 2) + '-' + pad(dd, 2));
     setAp(ap);
     setDow(dw);
+    setZulu(utcHh + utcMm + "Z");
   };
 
   useEffect(() => {
     setTimeout(updateTime, 0);
-    let refreshTimer: NodeJS.Timeout = setInterval(updateTime, 1000);
+    const refreshTimer: NodeJS.Timeout = setInterval(updateTime, 1000);
 
     return () => {
       clearInterval(refreshTimer);
@@ -43,6 +47,7 @@ export default function Clock() {
       <div className="time">{time}<span className="ampm">{ap}</span></div>
       <div className="date">{date}</div>
       <div className="dow">{dow}</div>
+      <div className="zulu">{zulu}</div>
     </div>
   )
 
